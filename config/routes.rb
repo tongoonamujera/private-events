b9e1e5411f9e1d37147bcf5e507f08bd4584bcc0
tongoonamujera <tongoonamujera@gmail.com>
2021-08-15 18:52:58 +0200

    Tweak routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :attendances
   resources :events
   devise_for :users
+  root 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
