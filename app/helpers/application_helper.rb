caf2bc08e79336bb577d6e1a114e36d7ff5deab4
tongoonamujera <tongoonamujera@gmail.com>
2021-08-16 16:37:32 +0200

    Edit events form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def event_registration_button(event)
     unless event.attendances.exists?(user_id: current_user.id)
-      (link_to 'Attend', new_attendance_path(id: event.id), class: 'button is-primary', method: :post).to_s.html_safe
+      (link_to 'Attend Event', new_attendance_path(id: event.id), class: 'button is-primary', method: :post).to_s.html_safe
     else
       '<p>You already signed up for this event</p>'.html_safe
     end
